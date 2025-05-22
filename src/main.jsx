8e8b7e247de4168a92fd16ed0cac50a84d48ca53
Sandip Nandy <138280329+sandipnandy1@users.noreply.github.com>
2025-05-22 12:04:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
     clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: 'https://sandipnandy1.github.io/TodoApp/'
     }}
     >
       <App />
